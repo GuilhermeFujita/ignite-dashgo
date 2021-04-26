94a86c95985c1e056582510ef8c300ef6e85224c
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-04-25 21:00:10 -0300

    Components organization

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,15 @@
+import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Guilherme Fujita</Text>
+        <Text color="gray.300" fontSize="small">fujitaguilherme@hotmail.com</Text>
+      </Box>
+
+      <Avatar size="md" name="Guilherme Fujita" src="https://github.com/GuilhermeFujita.png" />
+    </Flex>
+
+  );
+}
